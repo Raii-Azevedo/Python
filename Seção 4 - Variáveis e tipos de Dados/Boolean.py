da2e6032f53b512e1f2e77a3b5f309c74003f0e9
Raissa Azevedo <rhaii.azevedo@gmail.com>
2021-07-19 21:12:29 -0300

    Variáveis e Tipos de Dados

---
+++ Seção 4 - Variáveis e tipos de Dados/Boolean.py
@@ -0,0 +1,5 @@
+print('''O tipo Booleano
+Criado por George Boole
+2 constantes - Verdadeiro ou falso 
+Sempre com a inicial Maiuscula
+True -  False''')
---
+++ Seção 4 - Variáveis e tipos de Dados/exercícios/2.py
@@ -0,0 +1,3 @@
+print('1 - Faça um programa que leia um número real e o imprima')
+num = float(input('Digite um número real: '))
+print(f'O número foi: {num}')
